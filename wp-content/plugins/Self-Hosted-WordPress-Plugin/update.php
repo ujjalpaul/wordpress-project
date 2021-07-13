bb53bffb639068dda554f7031547c2408652ffe6
Ujjal <ujjalpaul67@gmail.com>
2021-07-13 19:58:36 +0530

    wp plugin add 2

--- wp-content/plugins/Self-Hosted-WordPress-Plugin/update.php
+++ wp-content/plugins/Self-Hosted-WordPress-Plugin/update.php
@@ -13,7 +13,7 @@ $obj = new stdClass();
 $obj->slug = 'plugin.php';  
 $obj->name = 'Plugin';
 $obj->plugin_name = 'plugin.php';
-$obj->new_version = '1.2';
+$obj->new_version = '1.1';
 // the url for the plugin homepage
 $obj->url = 'https://github.com/ujjalpaul/wordpress-project/tree/master/wp-content/plugins/';
 //the download location for the plugin zip file (can be any internet host)
